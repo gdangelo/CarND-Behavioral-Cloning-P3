b8c18ad3897fb28c9c28aafc5b653a29bd0e7e1b
Grégory D'Angelo <dangelo.gregory@gmail.com>
2017-10-09 16:29:52 +0200

    Build a basic deep neural network using Keras

--- model.py
+++ model.py
@@ -1,6 +1,17 @@
 import csv
 import cv2
 import numpy as np
+import tensorflow as tf
+
+from keras.models import Sequential
+from keras.layers import Input, Dense, Flatten
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+# command line flags
+flags.DEFINE_string('epoch', 10, "Number of epochs used for training")
+flags.DEFINE_string('batch_size', 32, "Batch size used for training")
 
 def load_data():
 	# Data
@@ -23,6 +34,27 @@ def load_data():
 	# Return numpy arrays
 	return (np.array(images), np.array(measurements))
 
-
-if __name__ == '__main__':
+def main(_):
+	# Import training data
 	X_train, y_train = load_data()
+	print(X_train.shape, y_train.shape)
+
+	# Build the model
+	model = Sequential()
+
+	model.add(Flatten(input_shape=X_train.shape[1:]))
+	model.add(Dense(1))
+
+	print(model.summary()) 
+
+	model.compile(optimizer='adam', loss='mse')
+
+	# Train the model
+	model.fit(X_train, y_train, batch_size=FLAGS.batch_size, nb_epoch=FLAGS.epoch, validation_split=0.2, shuffle=True)
+
+	# Save it
+	model.save('model.h5')
+
+# parses flags and calls the `main` function above
+if __name__ == '__main__':
+    tf.app.run()

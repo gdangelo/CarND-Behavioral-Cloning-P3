0c99e58fc03d226ff4efc31e43697a3d049e5549
Grégory D'Angelo <dangelo.gregory@gmail.com>
2017-10-10 11:58:13 +0200

    feat: Implement LeNet-5 model and grayscale images

---
+++ backup/README.md
@@ -0,0 +1,5 @@
+# History of the models built along the project
+
+1. **model_00.h5**: Build a simple Neural Network with only one fully-connected layer. Used to test the simulator.
+
+2. **model_01.h5**: Build LeNet-5 Neural Network without dropout or regularization techniques. Images has been converted to grayscale, resized to 32x32, and normalized.
--- model.py
+++ model.py
@@ -4,7 +4,9 @@ import numpy as np
 import tensorflow as tf
 
 from keras.models import Sequential
-from keras.layers import Input, Dense, Flatten, Lambda
+from keras.layers import Input, Dense, Flatten, Lambda, Activation
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -30,29 +32,73 @@ def load_data():
 			images.append(image)
 			# Load the steering angle
 			measurements.append(line[3])
+			# Augment data by flipping image around y-axis
+			#images.append(cv2.flip(image, 1))
+			#measurements.append(-line[3])
 
 	# Return numpy arrays
 	return (np.array(images), np.array(measurements))
+
+def grayscale(input):
+	from keras.backend import tf as ktf
+	return ktf.image.rgb_to_grayscale(input)
+
+def resize_image(input, w=32, h=32):
+	from keras.backend import tf as ktf
+	return ktf.image.resize_images(input, [w,h], ktf.image.ResizeMethod.BICUBIC)
+
+def normalize_image(input):
+	return (input / 255.0) - 0.5
+
+def build_lenet_model(data):
+	model = Sequential()
+
+	# --- Convert image into grayscale
+	model.add(Lambda(grayscale, input_shape=data.shape[1:]))
+	# --- Resize it to have a 32x32 shape
+	model.add(Lambda(resize_image))
+	# --- Normalize and mean center the data
+	model.add(Lambda(normalize_image))
+
+	# --- Layer 1 : Convolution + ReLu activation + maxpooling
+	model.add(Conv2D(filters=16, kernel_size=(5,5), strides=(1,1), padding='valid'))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2,2), padding='same'))
+
+	# --- Layer 2 : Convolution + ReLu activation + maxpooling
+	model.add(Conv2D(filters=6, kernel_size=(5,5), strides=(1,1), padding='valid'))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2,2), padding='same'))
+
+	# --- Flatten the weights
+	model.add(Flatten())
+
+	# --- Layer 3 : Fully-connected + ReLu activation
+	model.add(Dense(120))
+	model.add(Activation('relu'))
+
+	# --- Layer 4 : Fully-connected + ReLu activation
+	model.add(Dense(84))
+	model.add(Activation('relu'))
+
+	# --- Layer 5 : Fully-connected
+	model.add(Dense(1))
+
+	print(model.summary()) 
+
+	model.compile(optimizer='adam', loss='mse')	
+
+	return model
 
 def main(_):
 	# Import training data
 	X_train, y_train = load_data()
 	print(X_train.shape, y_train.shape)
 
-	# Build the model
-	model = Sequential()
-
-	model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=X_train.shape[1:]))
-	model.add(Flatten())
-	model.add(Dense(1))
-
-	print(model.summary()) 
-
-	model.compile(optimizer='adam', loss='mse')
-
+	# Build the model LeNet-5
+	model = build_lenet_model(X_train)
 	# Train the model
-	model.fit(X_train, y_train, batch_size=int(FLAGS.batch_size), nb_epoch=int(FLAGS.epoch), validation_split=0.2, shuffle=True)
-
+	model.fit(X_train, y_train, batch_size=int(FLAGS.batch_size), epochs=int(FLAGS.epoch), validation_split=0.2, shuffle=True)
 	# Save it
 	model.save('model.h5')
 
